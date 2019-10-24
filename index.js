5ac5d83c9c23dcf77581b8d9027d68443cfc6b42
Rasmus Meinecke <rasmusfabius83@gmai.com>
2019-10-24 16:37:46 +0200

    version 2

--- index.js
+++ index.js
@@ -10,5 +10,54 @@ function addMembers() {
 // - The user can set the number of teams
 function numbers() {
   var number = parseInt(document.querySelector("#numberOfTeams").value);
-  console.log(number);
+  var listTeams = document.querySelector("#teams");
+  var newLi = document.createElement("li");
+  newLi.className = "list-group-item";
+  newLi.textContent = number;
+  listTeams.append(newLi);
+}
+
+// - Every time the user clicks on the "assign" button, a random name is placed in a team and removed from the list
+
+// Get a list of names to an array.
+
+function makeArrayOfNames() {
+  var myList = document.querySelectorAll("#memberList li");
+  var toReturn = [];
+
+  for (var i = 0; i < myList.length; i++) toReturn.push(myList[i].innerText);
+
+  return toReturn;
+}
+
+// Radomise array
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+var arr = makeArrayOfNames();
+
+arr = shuffle(arr);
+console.log(arr);
+console.log(arr[0]);
+
+// remove name from list
+
+//- [EXTRA] Create a button to reset the state of the app
+function refreash() {
+  location.reload(true);
 }
